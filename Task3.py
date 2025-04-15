c1da1003ef74a9950e7a9610ddb73a581df38670
Akshay685as <akshay.mba09@iimamritsar.ac.in>
2025-04-15 13:08:27 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,9 @@
+
+# Step 1: Take input from user
+number = int(input("Enter a number: "))
+
+# Step 2: Check if number is even or odd using if-else
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
